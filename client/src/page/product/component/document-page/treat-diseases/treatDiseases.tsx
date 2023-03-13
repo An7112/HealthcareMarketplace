aa787539ba50ee012658de0de53ebcea1c64b765
An7112 <an.nguyent@gotecq.com>
2023-03-13 16:22:22 +0700

    update: item css

--- client/src/page/product/component/document-page/treat-diseases/treatDiseases.tsx
+++ client/src/page/product/component/document-page/treat-diseases/treatDiseases.tsx
@@ -1,32 +1,355 @@
 import React from 'react'
+import { BsFillCartCheckFill } from 'react-icons/bs'
+import { HiOutlineBadgeCheck } from 'react-icons/hi'
 import './treatDiseases.css'
 
 export const TreatDiseases = () => {
     return (
         <div className='document-all'>
             <div className='frame-item-list'>
-            <div className='item'>
-                {/* <img className='img-avatar profile' alt='' src='/media/profile.jpg' /> */}
-            </div>
-            <div className='item'>
-                {/* <img className='img-avatar profile' alt='' src='/media/profile.jpg' /> */}
-            </div>
-            <div className='item'>
-                {/* <img className='img-avatar profile' alt='' src='/media/profile.jpg' /> */}
-            </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
 
-            <div className='item'>
-                {/* <img className='img-avatar profile' alt='' src='/media/profile.jpg' /> */}
-            </div><div className='item'>
-                {/* <img className='img-avatar profile' alt='' src='/media/profile.jpg' /> */}
-            </div>
-            <div className='item'>
-                {/* <img className='img-avatar profile' alt='' src='/media/profile.jpg' /> */}
-            </div>
-            <div className='item'>
-                {/* <img className='img-avatar profile' alt='' src='/media/profile.jpg' /> */}
-            </div>
-            
+                    </div>
+                </div>
+
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div><div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='add-to-cart'>
+                        <BsFillCartCheckFill />
+                    </div>
+                    <div className='item-frame'>
+                        <span className='span-item'>
+                            <img className='img-item' alt='' src='/media/calcium-magnesium-zinc.png' />
+                        </span>
+                    </div>
+                    <div className='description-item'>
+                        <div className='flex-info-item'>
+                            <HiOutlineBadgeCheck />
+                            <p className='info-item'>
+                                Calcium magnesium zinc
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item desc'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div className='flex-info-item'>
+                            <p className='info-item'>
+                                0.001 ETH
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                
+
             </div>
 
         </div>
